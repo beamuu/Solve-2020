8f29450e27310ecaadca1fd5e37b23ca06d9a4c4
nutchanonc <edu.nutchanon@gmail.com>
2021-02-06 00:50:56 +0700

    update finished

--- jumppoint.cpp
+++ jumppoint.cpp
@@ -1,123 +1,97 @@
 #include <iostream>
+#include <vector>
 #include <list>
 #include <cmath>
+
 using namespace std;
 
-typedef struct NODE { int name,xpos,ypos; } node;
-list <node> list_of_node;
+struct node {int name,x,y;};
+
 list <int> queue;
-list <node>::iterator it1;
-list <node>::iterator it2;
-int res=0;
-int exit_status = 0;
-// 0 = possible
-// 1 = impossible
+vector <int> graph[1002];
+node nds[1002];
+int deg[1002];
+int dist[1002];
+int visited[1002];
 
-void BFS(int **dir , int *visited , int startNode , int endNode , int n) {
-    int exit=false;
-    int currentNode, currentLayer=1,nextLayer=0;
-    queue.push_back(startNode);
-    // printf("BFS searching for %d -> %d\n" , startNode , endNode);
-    while (true) {
-
-        if (queue.empty()) { exit_status = 1; break; }
-        currentNode = queue.front();
-        // printf("Visited node %d\n" , currentNode);
-        queue.pop_front();
-        // Mark as visited
-        visited[currentNode] = 1;
-        // Decreament layer.
-        currentLayer--;
-        // Find The available path(s)
-        for (int i=0 ; i<n+2 ; i++) {
-
-            if (currentNode != i && dir[currentNode][i] && !visited[i]) {
-                if (i == endNode) {
-                    exit = true;
-                    break;
-                }
-                //printf("    Find path from %d to %d\n" , currentNode , i);
-                queue.push_back(i);
-                nextLayer++;
-            }
-        }
-        //printf("Layer details ==== %d ==== %d ==== %d ====\n",currentLayer,nextLayer,res);
-        // Solved.
-        if (exit) {
-            res++;
-            break;
-        }
-        // Entire Layer discovered.
-        if (currentLayer == 0) { currentLayer = nextLayer ; nextLayer = 0; res++; }
-
-    }
-    if (exit_status) {
-        res = -1;
-    }
-    return;
+node createNode(int name , int x , int y) {
+    node newnode; newnode.name = name; newnode.x = x; newnode.y = y;
+    return newnode;
 }
 
+bool compare(int x1, int y1 , int x2 , int y2 , int radius) {
+    int sq_of_x = (x1-x2)*(x1-x2);
+    int sq_of_y = (y1-y2)*(y1-y2);
+    //printf("%d [%d %d %d %d]=====[] %d\n" , sq_of_x + sq_of_y ,x1 , y1 , x2 , y2, radius*radius);
+    if (sq_of_x + sq_of_y <= radius*radius) return true;
+    return false;
+}
 
-int main() {
+void init(int nodes, int r) {
 
-    // Declaration
-    int n,r,name=1,nloop;
-    int xpos , ypos;
-    cin >> n >> r;
-
-    // Mark as visited or unvisited.
-    int *visited = (int *)malloc(sizeof(int)*(n+2));
+    int x , y , name=1;
     
-    // Insert Start node
-    node newnode;
-    newnode.name = 0;
-    newnode.xpos = 0;
-    newnode.ypos = 0;
-    list_of_node.push_back(newnode);
+    nds[0] = createNode(0,0,0);
+    while (nodes--) {
+        cin >> x >> y;
 
-    // 2D Array Show Path of each node.
-    int **dir = (int **)malloc(sizeof(int *)*(n+2));
-    for (int i=0 ; i<n+2 ; i++) { dir[i] = (int *)malloc(sizeof(int)*(n+2)); }
-    nloop = n;
-    while (nloop--) {
-        cin >> xpos >> ypos;
-        node newnode;
-        newnode.name = name;
-        newnode.xpos = xpos;
-        newnode.ypos = ypos;
-        list_of_node.push_back(newnode);
+
+        nds[name] = createNode(name,x,y);
+        for (int i=0 ; i<name ; i++) {
+            //printf("name: (%d) =>?? (%d)" , name , i);
+            if (compare(x,y,nds[i].x,nds[i].y,r)) {
+                graph[name].push_back(nds[i].name);
+                graph[nds[i].name].push_back(name);
+                
+                deg[name]++;
+                deg[nds[i].name]++;
+            }
+        }
         name++;
     }
-
-    newnode.name = name;
-    newnode.xpos = 100;
-    newnode.ypos = 100;
-    list_of_node.push_back(newnode);
-
-    // Assign each of the path to all node;
-
-    for (it1 = list_of_node.begin() ; it1 != list_of_node.end() ; it1++) {
-        for (it2 = list_of_node.begin() ; it2 != list_of_node.end() ; it2++) {
-            if ( pow(it1->xpos-it2->xpos,2)+pow(it1->ypos-it2->ypos,2) <= pow(r,2) ) {
-                dir[it1->name][it2->name] = 1;
-                dir[it2->name][it1->name] = 1;
-            }
+    nds[name] = createNode(name,100,100);
+    for (int i=0 ; i<name ; i++) {
+        //printf("name: (%d) =>?? (%d)" , name , i);
+        if (compare(100,100,nds[i].x,nds[i].y,r)) {
+            graph[name].push_back(nds[i].name);
+            graph[nds[i].name].push_back(name);
+                
+            deg[name]++;
+            deg[nds[i].name]++;
         }
     }
-    /*
-    for (int i=0 ; i<n+2 ; i++) {
-        for (int j=0 ; j<n+2 ; j++) {
-            cout << dir[i][j];
-        }
-        cout << '\n';
-    }
-    */
-
-    // Process
-    BFS(dir,visited,0,n+1,n);
-    cout << res << endl;
-    return 0;
-    
-
 }
 
+void BFS() {
+    int current , next , layer;
+    queue.push_back(0);
+    
+    while (!queue.empty()) {
+        current = queue.front();
+        queue.pop_front();
+        visited[current] = 1;
+        layer = dist[current];
+        //printf("node: %d\n" , current);
+        for (int i=0 ; i<deg[current] ; i++) {
+            next = graph[current][i];
+            if (!dist[next] && !visited[next]) {
+                dist[next] = layer+1;
+                queue.push_back(next);
+            }
+            
+        }   
+    }
+}
+
+int main () {
+
+    int n , r;
+    cin >> n >> r;
+    init(n,r);
+    BFS();
+    if (!dist[n+1]) {
+      printf(-1);
+    } else printf("%d\n",dist[n+1]);
+
+    return 0;
+}
--- qualitymap.cpp
+++ qualitymap.cpp
@@ -1,44 +1,86 @@
 #include <iostream>
 using namespace std;
 
-int xpos,ypos;
+char map[30][30];
+bool visited[30][30];
+char water = '#';
+char forest = '*';
+char mineral = '$';
 
-void move(string *map , int **mark) {
-    mark[xpos][ypos] = 1;
+int count_forest  = 0;
+int count_mineral = 0;
+int all = 0;
+
+int high=0;
+int medium=0;
+
+void DFS(int n , int m , int i , int j) {
+    visited[i][j] = true;
+    all++;
+    //printf("At node:[%d][%d] , count_forest: %d , count_mineral: %d , all: %d\n" , i,j,count_forest,count_mineral,all);
+    // Check each elements
+    if (map[i][j] == forest) {
+        count_forest++;
+    }
+    else if (map[i][j] == mineral) {
+        count_mineral++;
+    }
     // North
-    if (xpos != 0 && !mark[xpos][ypos-1]) {
-        return north
+    if (i > 0 && map[i-1][j] != water) {
+        if (!visited[i-1][j]) {
+            DFS(n,m,i-1,j);
+        }
     }
     // South
+    if (i < n-1 && map[i+1][j] != water) {
+        if (!visited[i+1][j]) {
+            DFS(n,m,i+1,j);
+        }
+    }
     // East
+    if (j < m-1 && map[i][j+1] != water) {
+        if (!visited[i][j+1]) {
+            DFS(n,m,i,j+1);
+        }
+    }
     // West
+    if (j > 0 && map[i][j-1] != water) {
+        if (!visited[i][j-1]) {
+            DFS(n,m,i,j-1);
+        }
+    }
+    
 }
 
 int main() {
     
     int n,m;
     cin >> n >> m;
-    string map[n];
-    int **mark = (int **)malloc(sizeof(int *)*n);
-    for (int i=0 ; i<n ; i++) {
-        mark[i] = (int *)malloc(sizeof(int)*m);
-    }
-    
-    for (int i=0 ; i<n ; i++) {
-        cin >> map[i];
-    }
-    /*
     for (int i=0 ; i<n ; i++) {
         for (int j=0 ; j<m ; j++) {
-            cout << mark[i][j];
+            scanf(" %c",&map[i][j]);
         }
-        cout << endl;
-    }
-    */
-    while (1) {
-        move()
     }
 
-
+    for (int i=0 ; i<n ; i++) {
+        for (int j=0 ; j<m ; j++) {
+            if (!visited[i][j] && map[i][j] != water) {
+                DFS(n,m,i,j);
+                if (count_forest && count_mineral) {
+                    high+=all;
+                }
+                else if (count_forest || count_mineral) {
+                    medium+=all;
+                }
+                //printf("Done DFS with=> high:%d medium:%d\n",high,medium);
+                all = 0;
+                count_forest  = 0;
+                count_mineral = 0;
+            }
+        }
+    }
+    printf("%d %d\n" , high , medium );
     return 0;
 }
+
+
